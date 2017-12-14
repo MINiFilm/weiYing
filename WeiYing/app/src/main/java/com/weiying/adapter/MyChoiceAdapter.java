4ec921b7f86393de3c8d160087ec0671de2b6229
douyahaha <1372943215@qq.co>
2017-12-14 21:12:14 +0800

    ChoiceMVP

--- WeiYing/app/src/main/java/com/weiying/adapter/MyChoiceAdapter.java
+++ WeiYing/app/src/main/java/com/weiying/adapter/MyChoiceAdapter.java
@@ -1,32 +1,29 @@
 package com.weiying.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * Created by 杨圆圆 on 2017/12/14.
  */
 
-public class MyChoiceAdapter extends RecyclerView.Adapter<MyChoiceAdapter.MyChoiceViewHolder> {
+public class MyChoiceAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
-    public MyChoiceAdapter.MyChoiceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(MyChoiceAdapter.MyChoiceViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
     }
+
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 2;
     }
 
-    public class MyChoiceViewHolder extends RecyclerView.ViewHolder{
-        public MyChoiceViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
+
 }
