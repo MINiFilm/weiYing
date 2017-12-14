8e0448e484ab38d2704b7227ca25d4170ec14619
zhaoshanshan0906 <2410002148@qq.com>
2017-12-14 14:07:19 +0800

    zss FragmentFind.java 12/14

---
+++ WeiYing/app/src/main/java/com/weiying/fragment/FragmentFind.java
@@ -0,0 +1,23 @@
+package com.weiying.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 1.类的用途
+ * 2.@author棒棒糖：赵姗杉
+ * 3.@date2017/12/14  14：02
+ */
+
+public class FragmentFind extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+        
+    }
+}
