609fdec74c57a94334c566bf9e43905d908762af
douyahaha <1372943215@qq.co>
2017-12-14 16:41:33 +0800

    动画

---
+++ WeiYing/app/src/main/java/com/weiying/actiity/IntoActivity.java
@@ -0,0 +1,40 @@
+package com.weiying.actiity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.weiying.R;
+
+import butterknife.Bind;
+
+public class IntoActivity extends AppCompatActivity {
+    @Bind(R.id.iv_into)
+    ImageView iv_into;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_into);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.scale);
+        iv_into.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(IntoActivity.this,MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
