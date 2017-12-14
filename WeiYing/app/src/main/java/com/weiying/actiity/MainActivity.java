18e5a1afcede46e47472d88c110126f5c0034c90
Aries10 <1374960689@qq.com>
2017-12-14 15:41:54 +0800

    总的

--- WeiYing/app/src/main/java/com/weiying/actiity/MainActivity.java
+++ WeiYing/app/src/main/java/com/weiying/actiity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.FragmentManager;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.weiying.R;
 import com.weiying.fragment.FragmentChoice;
@@ -17,8 +18,11 @@ import butterknife.ButterKnife;
 public class MainActivity extends BaseActivity {
     @Bind(R.id.rg)
     RadioGroup rg;
+    @Bind(R.id.tv)
+    TextView tv;
 
     private FragmentManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,18 +32,22 @@ public class MainActivity extends BaseActivity {
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-                switch (checkedId){
+                switch (checkedId) {
                     case R.id.rb1:
-                        manager.beginTransaction().replace(R.id.fram,new FragmentChoice()).commit();
+                        manager.beginTransaction().replace(R.id.fram, new FragmentChoice()).commit();
+                        tv.setText("精选");
                         break;
                     case R.id.rb2:
-                        manager.beginTransaction().replace(R.id.fram,new FragmentSpecial()).commit();
+                        manager.beginTransaction().replace(R.id.fram, new FragmentSpecial()).commit();
+                        tv.setText("专题");
                         break;
                     case R.id.rb3:
-                        manager.beginTransaction().replace(R.id.fram,new FragmentFind()).commit();
+                        manager.beginTransaction().replace(R.id.fram, new FragmentFind()).commit();
+                        tv.setText("发现");
                         break;
                     case R.id.rb4:
-                        manager.beginTransaction().replace(R.id.fram,new FragmentMine()).commit();
+                        manager.beginTransaction().replace(R.id.fram, new FragmentMine()).commit();
+                        tv.setText("我的");
                         break;
                 }
             }
