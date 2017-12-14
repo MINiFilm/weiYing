e631523eb9bbedcefbe73ce1d3f94383244907f0
douyahaha <1372943215@qq.co>
2017-12-14 14:10:48 +0800

    精选

---
+++ WeiYing/app/src/main/java/com/weiying/fragment/FragmentChoice.java
@@ -0,0 +1,20 @@
+package com.weiying.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 杨圆圆 on 2017/12/14.
+ */
+
+public class FragmentChoice extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
