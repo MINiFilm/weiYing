274a0fc61fdf17ce7e0095e1c07b5e17b4ac3f0b
Aries10 <1374960689@qq.com>
2017-12-14 15:59:59 +0800

    包

--- WeiYing/app/src/main/java/com/weiying/mvp/SpecialModle.java
+++ WeiYing/app/src/main/java/com/weiying/mvp/SpecialModle.java
@@ -1,8 +1,13 @@
 package com.weiying.mvp;
 
+import com.weiying.bean.ChoiceBean;
+
+import io.reactivex.Flowable;
+
 /**
  * Created by dell-pc on 2017/12/14.
  */
 
 public interface SpecialModle {
+     Flowable<ChoiceBean> getData();
 }
